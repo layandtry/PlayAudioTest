a511f4cb1f877992ce5bdcdef89e55f9914d1964
layandtry <lay_and_try@163.com>
2018-05-29 21:09:29 +0800

    Initial commit

---
+++ app/src/main/java/com/wlw_02_19_wangmiao/playaudiotest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wlw_02_19_wangmiao.playaudiotest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
